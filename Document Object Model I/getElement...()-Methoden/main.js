0cf4733d1a0cf00cbb96bb15a69680869ef2cc24
Altiuss <altiuss@inbox.lv>
2025-01-20 09:06:49 +0100

    added querySelector()

---
+++ Document Object Model I/getElement...()-Methoden/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let el1 = document.getElementById("navigation");
+console.log(el1);
+
+
+let el2 = document.getElementById("test");
+console.log(el2);
+
+let els = document.getElementsByClassName("jumbotron");
+console.log(els);
+
+let els1 = document.getElementsByClassName("jumbo");
+console.log(els1);
+
+let elTagName = document.getElementsByTagName("p");
+console.log(elTagName);
---
+++ Document Object Model I/querySelector...()-Methoden/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let qs1 = document.querySelector("body > #navigation > ul");
+
+console.log(qs1);
+
+
+let qs2 = document.querySelector(".jumbotron");
+console.log(qs2);
+
+let qs3 = document.querySelectorAll(".jumbotron");
+console.log(qs3);
+
+let qs4 = document.querySelectorAll("body > #navigation > ul >  li > a");
+console.log(qs4);
