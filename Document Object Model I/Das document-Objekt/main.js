93be9c1ea4242156bb3cb79260d9d14b3905c96e
Altiuss <altiuss@inbox.lv>
2025-01-20 07:58:17 +0100

    Das DOM Eigenschaften und Methoden

---
+++ Document Object Model I/Das document-Objekt/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.location);
+console.log(document.ondblclick);
+console.log(document.URL);
+console.log(document.ondblclick);
+// console.log(document.location.href = "http://www.google.com");
