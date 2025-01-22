48ec282f39f6987ed6bbc6a80a68c60cb3dc97da
Altiuss <altiuss@inbox.lv>
2025-01-22 17:30:58 +0100

    elemente entfernen

---
+++ Document Object Model I/Elemente entfernen/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let nav = document.querySelector("#navigation");
+
+// nav.remove();
+let jumbo = document.querySelectorAll(".jumbotron");
+// jumbo.remove();
+
+jumbo.forEach(function(e) {
+    e.remove();
+});
+
+console.log(nav);
+console.log(jumbo);
