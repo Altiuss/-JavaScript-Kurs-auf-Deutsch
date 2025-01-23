50318c8f04f8c8607a7c911dfac285f0e02e8984
Altiuss <altiuss@inbox.lv>
2025-01-23 16:51:33 +0100

    Die style_Eigenschaft

---
+++ Document Object Model I/Die style-Eigenschaft/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let jumbo = document.querySelector(".jumbotron");
+jumbo.style.backgroundColor = "red",
+       
+console.log(jumbo.style);
+console.log(jumbo.style.backgroundColor);
+
+jumbo.style.width = "80%";
+jumbo.style.hight = "1000%";
