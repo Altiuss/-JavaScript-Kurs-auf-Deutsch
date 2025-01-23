4ff6acf168207103b410b5ce53e39db90bc3e78a
Altiuss <altiuss@inbox.lv>
2025-01-23 17:12:59 +0100

    Das window-Object

---
+++ Document Object Model I/Das window-Objekt/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(window);
+console.log(window.history);
+console.log(window.alert);
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(innerHeight);
+console.log(outerHeight);
+console.log(outerWidth);
+console.log(scrollX);
+console.log(scrollY);
+console.log(location);
+console.log(localStorage);
+console.log(sessionStorage);
