c70460e55d5eb31bae552cf280c0a2163cd23fa9
Altiuss <altiuss@inbox.lv>
2025-01-23 18:10:44 +0100

    Die getCompjutedStyles()-Methode

---
+++ Document Object Model I/Die getComputedStyles()-Methode/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let jumbo = document.querySelector(".jumbotron");
+let jubboStyle = getComputedStyle(jumbo);
+console.log(jubboStyle);
+console.log(jubboStyle.color);
+console.log(jubboStyle.color);
+console.log(jubboStyle.backgroundColor);
+console.log(jubboStyle.fontFamily);
+console.log(jubboStyle.animation);
