7e83073cd7e19b256ecd643dddd006dcffde5f6d
Altiuss <altiuss@inbox.lv>
2025-01-23 18:26:26 +0100

    Das location-Objekt

---
+++ Document Object Model I/Das location-Objekt/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+console.log(location);
+console.log(location.href);
+// location.href = "https://www.google.com";
+
+console.log(location.protocol);
+console.log(location.hostname);
+console.log(location.pathname);
+console.log(location.search);
+console.log(location.hash);
+console.log(location.hash);
+
+// location.reload();
+
+// location.assign("http://www.google.com");
+
+location.replace("http://www.google.com");
+
