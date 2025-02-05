887f89a1399348e8b677bb186140f7fc939be94e
Altiuss <altiuss@inbox.lv>
2025-02-05 10:24:19 +0100

    Die Attribute async und defer

---
+++ Expertenwissen/Die Attribute async und defer/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let div = document.querySelector("#test");
+
+
+if (div) {
+    console.log("Das <DIV>-Element wurde gefunden");
+} else {
+    console.log("War wohl nicht da");
+}
