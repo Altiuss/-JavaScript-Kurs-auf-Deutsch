dcbe6ea56d31b335046c990a4b36b1d348cf622d
Altiuss <altiuss@inbox.lv>
2025-02-10 19:02:00 +0100

    Klassen erweitern

---
+++ Klassen/Klassen erweitern/Kinderkonto.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+class Kinderkonto extends Konto {
+
+    constructor(iban, inhaber, einzahlung, limit) {
+        super(iban, inhaber, einzahlung)
+        this._limit = limit;
+    }
+
+}
