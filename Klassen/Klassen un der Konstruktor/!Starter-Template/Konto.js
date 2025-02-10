fd22450a7eb90e19ca5a4d598a5b47765a4a2060
Altiuss <altiuss@inbox.lv>
2025-02-10 13:22:02 +0100

    Klassen und der Konstruktor

---
+++ Klassen/Klassen un der Konstruktor/!Starter-Template/Konto.js
@@ -0,0 +1,12 @@
+"use strict"
+
+class Konto {
+    constructor(iban, inhaber, einzahlung) {
+        this.iban = iban;
+        this.inhaber = [inhaber];
+        this.saldo = this.saldo;
+        this.aktiv = true;
+    }
+
+};
+
