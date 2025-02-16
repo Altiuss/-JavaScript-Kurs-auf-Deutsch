7459f896ef51bea4f89a1295ed6fe13c97240bc2
Altiuss <altiuss@inbox.lv>
2025-02-16 19:15:23 +0100

    cookies Teil II

--- Cookies und Daten-Speichern/Cookies/main.js
+++ Cookies und Daten-Speichern/Cookies/main.js
@@ -1,19 +1,29 @@
 "use strict";
 
+//Syntax
+// document.cookie = "cookie_name=[cookie_wert][; max-age-zeit_in_s]"
 
+document.cookie = "vorname=Aigars";
+document.cookie = "nacname=ALutis";
+document.cookie = `${encodeURIComponent("ein key; value-paare")}=${encodeURIComponent("key=value")}`;
+document.cookie = "10s_cookie=; max-age=10";
 
-// document.cookie = "vorname=Aigars";
-// document.cookie = "nacname=ALutis";
-// document.cookie = `${encodeURIComponent("ein key; value-paare")}=${encodeURIComponent("key=value")}`;
+setTimeout(() => console.log(document.cookie), 9000);
+setTimeout(() => console.log(document.cookie), 11000);
 
-const set_cookie = function(name, wert){
+document.cookie = `mein_alter=49; max-age=${60*60*24}`;
+
+const set_cookie = function(name, wert, haltbarkeit){
     let cookie = `${encodeURIComponent(name)}=`;
     if(wert ) {
         cookie += `${encodeURIComponent(wert)}`;
     }
+    if(haltbarkeit) {
+        cookie += `; max-age=${haltbarkeit}`;
+    }
     document.cookie = cookie;
 };
 
-set_cookie("passwort", "@#$%$%^^HHGG*88");
+set_cookie("passwort", "@#$%$%^^HHGG*88", 60*60*2);
 
-console.log(document.cookie);
+// console.log(document.cookie);
